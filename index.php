0890af7f46dc06d003cc62ec8b846b79f3d36d9b
Rodrigo Dalfre <67020557+rodrigodalfre@users.noreply.github.com>
2022-02-07 19:14:26 -0300

    php/js/css

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
+<?php 
+    require "app/apicontroller.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="public/css/style.css">
     
+    <script src="public/js/script.js"></script>
     <title>Covid Status</title>
 </head>
 <body>
     
+    <h1>Covidão Status</h1>
+
 </body>
 </html>
