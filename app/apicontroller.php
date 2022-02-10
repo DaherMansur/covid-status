22bb1ff76c5a95db491f50e21ab5b8331747358f
Rodrigo Dalfre <67020557+rodrigodalfre@users.noreply.github.com>
2022-02-10 07:17:23 -0300

    Funçcao getAll(todas infos) e inicio para outras requisicoes

--- app/apicontroller.php
+++ app/apicontroller.php
@@ -1,28 +1,64 @@
 <?php
 
-
-$curl = curl_init();
-$url = 'https://api.covid19api.com/country/brazil/status/confirmed';
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $url,
-  CURLOPT_RETURNTRANSFER => true
-));
-
-$res = curl_exec($curl);
-
-curl_close($curl);
-
-$response = json_decode($res, true);
 echo '<pre>';
+class CovidStatus {
+  
+  public function request($endpoint, $params = array()){
 
-#Criar class, criar requisição do que é necessário
+    $curl = curl_init();
+    $url = 'https://api.covid19api.com/' .$endpoint. '?';
 
-foreach($response as $key){
-  echo $key['Cases'].'<br>';
+    // Query String = Data format. 
+    //?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z
+
+    #Tratar isso mais tarde
+    if(is_array($params)){
+      foreach($params as $param => $value){
+        if(empty($params)) continue;
+        $url .= $param . urlencode($value);
+      }
+    }
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $url,
+      CURLOPT_RETURNTRANSFER => true
+    ));
+
+    $res = curl_exec($curl);
+    curl_close($curl);
+
+    return json_decode($res, true);
+  }
+
+  public function getTotal($endpoint){
+    $data = $this->request($endpoint);
+
+    if(empty($data) && !is_array($data)){
+        return false;
+    }
+    return end($data);
+  }
+
+  public function getDeath($endpoint, $date = ''){
+    $data = $this->request($endpoint, $date);
+    $deaths = [];
+
+    foreach($data as $key){
+      $deaths[] = $key['Deaths'];
+    }
+    #print_r($data);
+    print_r($deaths);
+  }
+
 }
 
+$covidStatus = new CovidStatus();
+$deaths = $covidStatus->getDeath('country/brazil');
+#$total = $covidStatus->getTotal('country/brazil');
+// print_r($deaths);
 
+#echo 'Houve um total de: '. $total['Confirmed'];
+echo '<br>';
 
 
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
-    // require "app/apicontroller.php";
+    // require_once "app/apicontroller.php";
 
+    echo '<pre>';
+
+    print_r($_GET);
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +13,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <link rel="stylesheet" href="public/css/style.css">
-    <script src="https://code.highcharts.com/highcharts.js"></script> <!-- highcharts.js --> 
+    <!-- Highcharts.js -->
+    <script src="https://code.highcharts.com/highcharts.js"></script>  
     
     <script src="public/js/script.js"></script>
     <title>Covid Status</title>
