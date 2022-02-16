9b0698cfa64d52b23af6294b23664fb7b3e0e2bf
Garufa1 <99214332+SrGarufa@users.noreply.github.com>
2022-02-15 21:40:56 -0300

    .

--- index.php
+++ index.php
@@ -13,6 +13,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <link rel="stylesheet" href="public/css/style.css">
+    <?php
+        header("refresh:1");
+    ?>
     <!-- Highcharts.js -->
     <script src="https://code.highcharts.com/highcharts.js"></script>      
     <script src="public/js/script.js"></script>
