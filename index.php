887efcda45eb7f3fac607b1eec3d41ef1b3f648c
Garufa1 <99214332+SrGarufa@users.noreply.github.com>
2022-03-11 06:55:00 -0300

    ++overflow e scroll nav

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-<?php 
+<?php
 
-    $country = 'brazil';
-    require "app/config.php";
+$country = 'brazil';
+require "app/config.php";
 
-    #from=2021-01-01&to=2021-10-21
-    $avgConfirmed = $covidStatus->averageWeeks('Confirmed'); //Media semanal (Confirmados)
-    $avgDeaths = $covidStatus->averageWeeks('Deaths'); //Media semanal (Mortes)
+#from=2021-01-01&to=2021-10-21
+$avgConfirmed = $covidStatus->averageWeeks('Confirmed'); //Media semanal (Confirmados)
+$avgDeaths = $covidStatus->averageWeeks('Deaths'); //Media semanal (Mortes)
 
-    $countries = $covidStatus->getCountry(); //Lista de países(nome, slug, IO)
-    $totalCountry = $covidStatus->getTotalCountry(); //Listagem total de país
-    
-    $newCasesConfirmed = $covidStatus->newCases('Confirmed'); //Novos casos confirmados(perDay)
-    $newCasesDeaths = $covidStatus->newCases('Deaths'); //Novas mortes confirmadas(perDay)
+$countries = $covidStatus->getCountry(); //Lista de países(nome, slug, IO)
+$totalCountry = $covidStatus->getTotalCountry(); //Listagem total de país
 
-    /* foreach ($average as $key){
+$newCasesConfirmed = $covidStatus->newCases('Confirmed'); //Novos casos confirmados(perDay)
+$newCasesDeaths = $covidStatus->newCases('Deaths'); //Novas mortes confirmadas(perDay)
+
+/* foreach ($average as $key){
         echo '['.json_encode($key['Date']).','.json_encode($key['Confirmed']).'],' ;
     } */
 ?>
@@ -28,7 +28,7 @@
     <link rel="stylesheet" href="public/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <?php
-       /*  header("refresh:1"); */
+    /*  header("refresh:1"); */
     ?>
     <!-- Highcharts.js -->
     <!--     <script src="https://code.highcharts.com/highcharts.js"></script> -->
@@ -43,22 +43,24 @@
 
             </div>
 
-            <?php foreach($countries as $key) { ?>
-            <a href="?country=<?=$key['Slug']?>">
-                <div class="button-1">
-                    <div class="country">
-                        <ul>
-                            <li><?= $key['Country'] ?></li>
-                        </ul>
-                    </div>
-                    <div class="country-nmbrs">
-                        <ul>
-                            <li>99999</li>
-                        </ul>
-                    </div>
-                </div>
-            </a>
-            <?php } ?>
+            <nav>
+                <?php foreach ($countries as $key) { ?>
+                    <a href="?country=<?= $key['Slug'] ?>">
+                        <div class="button-1">
+                            <div class="country">
+                                <ul>
+                                    <li><?= $key['Country'] ?></li>
+                                </ul>
+                            </div>
+                            <div class="country-nmbrs">
+                                <ul>
+                                    <li>99999</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </a>
+                <?php } ?>
+            </nav>
         </aside>
         <header class="head">
             <h1>Covid Status</h1>
@@ -82,26 +84,26 @@
 
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script>
-    Highcharts.chart('graph-1', {
-        xAxis: {
-            
-        },
+        Highcharts.chart('graph-1', {
+            xAxis: {
 
-        plotOptions: {
-            series: {
-                dataLabels: {
-                    enabled: false
+            },
+
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        enabled: false
+                    }
                 }
-            }
-        },
+            },
 
-        series: [{
-            data: [<?php foreach($avgConfirmed as $key){
-                echo '['.json_encode($key['Date']).','.json_encode($key['Confirmed']).'],'; 
-            } ?>]
-        }]
-        
-    });
+            series: [{
+                data: [<?php foreach ($avgConfirmed as $key) {
+                            echo '[' . json_encode($key['Date']) . ',' . json_encode($key['Confirmed']) . '],';
+                        } ?>]
+            }]
+
+        });
     </script>
 
     <script src="public/js/script.js"></script>
