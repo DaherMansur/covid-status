de3ea1f40431392ce2629594899da64c3543c9ea
Garufa1 <99214332+SrGarufa@users.noreply.github.com>
2022-03-13 17:06:09 -0300

    alteracoes no Main

--- public/js/script.js
+++ public/js/script.js
@@ -11,7 +11,7 @@ let highcharts_bg = document.querySelector('.highcharts-background');
 
 
 btn_.addEventListener("click", function bgcolor(){
-    if(colorScheme < 3){
+    if(colorScheme <1){
         colorScheme++ 
     }else{
         colorScheme = 0;
@@ -26,32 +26,12 @@ btn_.addEventListener("click", function bgcolor(){
             footer_.style.backgroundColor = "#1e2140";
             break
         case 1:
-            body_.style.backgroundColor = "#FFF";
-            main_.style.backgroundColor = "#FFF";
-            container_.style.backgroundColor = "#FFF";            
-            aside_.style.backgroundColor = "#734EBC";
-            footer_.style.backgroundColor = "#734EBC";
-            break
-        case 2:
-            body_.style.backgroundColor = "#C7C3F5";
-            main_.style.backgroundColor = "#C7C3F5";
-            container_.style.backgroundColor = "#C7C3F5";
-            aside_.style.backgroundColor = "#734EBC";
-            footer_.style.backgroundColor = "#734EBC";
-            break
-        case 3:
             body_.style.backgroundColor = "#c1c1c1";
             main_.style.backgroundColor = "#c1c1c1";
             container_.style.backgroundColor = "#c1c1c1";
             aside_.style.backgroundColor = "#2D2D2D";
             footer_.style.backgroundColor = "#2D2D2D";
             break
-        case 4:
-            body_.style.backgroundColor = "#E2EDFF";
-            main_.style.backgroundColor = "#E2EDFF";
-            container_.style.backgroundColor = "#E2EDFF";
-            aside_.style.backgroundColor = "#5D80B8";
-            footer_.style.backgroundColor = "#5D80B8";
-        break
     }
 });
+
