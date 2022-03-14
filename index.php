edc1d1e86225895f5ca76c48944c1ef3b7abc05b
Garufa1 <99214332+SrGarufa@users.noreply.github.com>
2022-03-13 23:24:02 -0300

    ajustes layout

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ $newCasesDeaths = $covidStatus->newCases('Deaths'); //Novas mortes confirmadas(p
                         <div class="button-1">
                             <div class="country">
                                 <ul>
-                                    <li><?= $key['Country'] ?></li>
+                                    <li><p><?= $key['Country'] ?></p></li>
                                 </ul>
                             </div>
                             <div class="country-nmbrs">
@@ -94,8 +94,6 @@ $newCasesDeaths = $covidStatus->newCases('Deaths'); //Novas mortes confirmadas(p
                 <div id="graph-1"></div>
                 <div class="graph-descript">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
             </div>
-            <div class="content"></div>
-            <div class="content"></div>
             <div class="content"></div>
             <div class="content"></div>
             <div class="content"></div>
