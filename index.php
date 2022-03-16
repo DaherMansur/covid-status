788aa1920ef29667f5d4ad2e96c84e376a2938e6
Rodrigo Dalfre <67020557+rodrigodalfre@users.noreply.github.com>
2022-03-16 17:14:23 -0300

    grafico teste 2

--- index.php
+++ index.php
@@ -105,6 +105,10 @@ $date = date('Y-m-d', strtotime('-3 month'));
                 <div id="graph-1"></div>
                 <div class="graph-descript">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
             </div>
+            <div class="head-content flex column">
+                <div id="graph-2"></div>
+                <div class="graph-descript">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
+            </div>
             <div class="content"></div>
             <div class="content"></div>
             <div class="content"></div>
@@ -135,8 +139,26 @@ $date = date('Y-m-d', strtotime('-3 month'));
                             echo '[' . json_encode($key['Date']) . ',' . json_encode($key['Confirmed']) . '],';
                         } ?>]
             }]
+        })
+        Highcharts.chart('graph-2', {
+            xAxis: {
 
-        });
+            },
+
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        enabled: false
+                    }
+                }
+            },
+
+            series: [{
+                data: [<?php foreach ($avgDeaths as $key) {
+                            echo '[' . json_encode($key['Date']) . ',' . json_encode($key['Deaths']) . '],';
+                        } ?>]
+            }]
+        })
     </script>
 
     <script>
