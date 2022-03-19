f99452669f9a15c88e4cdce6996e3c5e95624499
Garufa1 <99214332+SrGarufa@users.noreply.github.com>
2022-03-19 03:34:21 -0300

    ++main, section, footer

--- index.php
+++ index.php
@@ -48,11 +48,36 @@ $date = date('Y-m-d', strtotime('-3 month'));
 </head>
 
 <body class="color-2">
-    <header class="flex row row-center column-center color-1"><a href=""><img src="/public/images/logo.png" alt="Covid Status"/></a></header>
+    <header class="flex row row-center column-center color-1">
+        <a href="">
+            <img src="/public/images/logo.png" alt="Covid Status" />
+        </a>
+    </header>
+    <div id="container" class="margin-auto">
+        <section class="flex column">
+            <div id="graph-1"></div>
+            <div class="graph-descript text-color-2"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p></div>
+        </section>
+        <section class="flex column">
+            <div id="graph-2"></div>
+            <div class="graph-descript text-color-2"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p></div>
+        </section>
+        <section class="flex column">
+            <div id="graph-3"></div>
+            <div class="graph-descript text-color-2"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p></div>
+        </section>
+        <section class="flex column">
+            <div id="graph-4"></div>
+            <div class="graph-descript text-color-2"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p></div>
+        </section>
+    </div>
+    <footer class="flex column column-center row-center color-1 text-color-1">
+        <p>References and credits ~</p>
+    </footer>
 
 
 
-        <!-- <aside class="nav">
+    <!-- <aside class="nav">
             <div id="nav-head" class="text-color">
                 <h3>Ajude a diminuir o contágio!</h3>
                 <div class="prev">
@@ -103,12 +128,12 @@ $date = date('Y-m-d', strtotime('-3 month'));
                 <?php } ?>
             </nav>
         </aside> -->
-       <!--   <header class="head">
+    <!--   <header class="head">
             <h1>Covid Status</h1>
             <button id="btn_">C</button>
             <button id="dateTime" value="<?= $date ?>">3 Months</button>
         </header> -->
-        <!-- <main class="grid container-main center">
+    <!-- <main class="grid container-main center">
             <div class="head-content flex column">
                 <div id="graph-1"></div>
                 <div class="graph-descript">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
@@ -130,236 +155,236 @@ $date = date('Y-m-d', strtotime('-3 month'));
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script>
         Highcharts.chart('graph-1', {
-        chart: {
-            zoomType: 'x'
-        },
-        title: {
-            text: 'Novos casos confirmados por dia'
-        },
-        subtitle: {
-            text: document.ontouchstart === undefined ?
-                'Selecione a área para dar zoom' : 'Pinch the chart to zoom in'
-        },
-        xAxis: {
-            type: ''
-        },
-        yAxis: {
+            chart: {
+                zoomType: 'x'
+            },
             title: {
-                text: 'Novos casos'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            area: {
-                fillColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
+                text: 'Novos casos confirmados por dia'
+            },
+            subtitle: {
+                text: document.ontouchstart === undefined ?
+                    'Selecione a área para dar zoom' : 'Pinch the chart to zoom in'
+            },
+            xAxis: {
+                type: ''
+            },
+            yAxis: {
+                title: {
+                    text: 'Novos casos'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[1]],
+                            [1, Highcharts.color(Highcharts.getOptions().colors[1]).setOpacity(0).get('rgba')]
+                        ]
                     },
-                    stops: [
-                        [0, Highcharts.getOptions().colors[1]],
-                        [1, Highcharts.color(Highcharts.getOptions().colors[1]).setOpacity(0).get('rgba')]
-                    ]
-                },
-                marker: {
-                    radius: 2
-                },
-                lineWidth: 1,
-                states: {
-                    hover: {
-                        lineWidth: 1
-                    }
-                },
-                threshold: null
-            }
-        },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
 
-        series: [{
-            type: 'area',
-            name: 'casos',
-            data: [<?php foreach ($newCasesConfirmed as $key) {
+            series: [{
+                type: 'area',
+                name: 'casos',
+                data: [<?php foreach ($newCasesConfirmed as $key) {
                             echo '[' . json_encode($key['Date']) . ',' . json_encode($key['Confirmed']) . '],';
                         } ?>]
-        }]
+            }]
         });
         Highcharts.chart('graph-2', {
-        chart: {
-            zoomType: 'x'
-        },
-        title: {
-            text: 'Média de casos por semana'
-        },
-        subtitle: {
-            text: document.ontouchstart === undefined ?
-                'Selecione a área para dar zoom' : 'Pinch the chart to zoom in'
-        },
-        xAxis: {
-            type: ''
-        },
-        yAxis: {
+            chart: {
+                zoomType: 'x'
+            },
             title: {
-                text: 'Média de casos'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            area: {
-                fillColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
+                text: 'Média de casos por semana'
+            },
+            subtitle: {
+                text: document.ontouchstart === undefined ?
+                    'Selecione a área para dar zoom' : 'Pinch the chart to zoom in'
+            },
+            xAxis: {
+                type: ''
+            },
+            yAxis: {
+                title: {
+                    text: 'Média de casos'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[1]],
+                            [1, Highcharts.color(Highcharts.getOptions().colors[1]).setOpacity(0).get('rgba')]
+                        ]
                     },
-                    stops: [
-                        [0, Highcharts.getOptions().colors[1]],
-                        [1, Highcharts.color(Highcharts.getOptions().colors[1]).setOpacity(0).get('rgba')]
-                    ]
-                },
-                marker: {
-                    radius: 2
-                },
-                lineWidth: 1,
-                states: {
-                    hover: {
-                        lineWidth: 1
-                    }
-                },
-                threshold: null
-            }
-        },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
 
-        series: [{
-            type: 'area',
-            name: 'casos',
-            data: [<?php foreach ($avgConfirmed as $key) {
+            series: [{
+                type: 'area',
+                name: 'casos',
+                data: [<?php foreach ($avgConfirmed as $key) {
                             echo '[' . json_encode($key['Date']) . ',' . json_encode($key['Confirmed']) . '],';
                         } ?>]
-        }]
+            }]
         });
 
         Highcharts.chart('graph-3', {
-        chart: {
-            zoomType: 'x'
-        },
-        title: {
-            text: 'Mortes confirmadas por dia'
-        },
-        subtitle: {
-            text: document.ontouchstart === undefined ?
-                'Selecione a área para dar zoom' : 'Pinch the chart to zoom in'
-        },
-        xAxis: {
-            type: ''
-        },
-        yAxis: {
+            chart: {
+                zoomType: 'x'
+            },
             title: {
-                text: 'Mortes'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            area: {
-                fillColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
+                text: 'Mortes confirmadas por dia'
+            },
+            subtitle: {
+                text: document.ontouchstart === undefined ?
+                    'Selecione a área para dar zoom' : 'Pinch the chart to zoom in'
+            },
+            xAxis: {
+                type: ''
+            },
+            yAxis: {
+                title: {
+                    text: 'Mortes'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[1]],
+                            [1, Highcharts.color(Highcharts.getOptions().colors[1]).setOpacity(0).get('rgba')]
+                        ]
                     },
-                    stops: [
-                        [0, Highcharts.getOptions().colors[1]],
-                        [1, Highcharts.color(Highcharts.getOptions().colors[1]).setOpacity(0).get('rgba')]
-                    ]
-                },
-                marker: {
-                    radius: 2
-                },
-                lineWidth: 1,
-                states: {
-                    hover: {
-                        lineWidth: 1
-                    }
-                },
-                threshold: null
-            }
-        },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
 
-        series: [{
-            type: 'area',
-            name: 'Mortes',
-            data: [<?php foreach ($newCasesDeaths as $key) {
+            series: [{
+                type: 'area',
+                name: 'Mortes',
+                data: [<?php foreach ($newCasesDeaths as $key) {
                             echo '[' . json_encode($key['Date']) . ',' . json_encode($key['Deaths']) . '],';
                         } ?>]
-        }]
+            }]
         });
 
 
 
         Highcharts.chart('graph-4', {
-        chart: {
-            zoomType: 'x'
-        },
-        title: {
-            text: 'Média de mortes confirmadas por semana'
-        },
-        subtitle: {
-            text: document.ontouchstart === undefined ?
-                'Selecione a área para dar zoom' : 'Pinch the chart to zoom in'
-        },
-        xAxis: {
-            type: ''
-        },
-        yAxis: {
+            chart: {
+                zoomType: 'x'
+            },
             title: {
-                text: 'Mortes'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            area: {
-                fillColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
+                text: 'Média de mortes confirmadas por semana'
+            },
+            subtitle: {
+                text: document.ontouchstart === undefined ?
+                    'Selecione a área para dar zoom' : 'Pinch the chart to zoom in'
+            },
+            xAxis: {
+                type: ''
+            },
+            yAxis: {
+                title: {
+                    text: 'Mortes'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[1]],
+                            [1, Highcharts.color(Highcharts.getOptions().colors[1]).setOpacity(0).get('rgba')]
+                        ]
                     },
-                    stops: [
-                        [0, Highcharts.getOptions().colors[1]],
-                        [1, Highcharts.color(Highcharts.getOptions().colors[1]).setOpacity(0).get('rgba')]
-                    ]
-                },
-                marker: {
-                    radius: 2
-                },
-                lineWidth: 1,
-                states: {
-                    hover: {
-                        lineWidth: 1
-                    }
-                },
-                threshold: null
-            }
-        },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
 
-        series: [{
-            type: 'area',
-            name: 'Mortes',
-            data: [<?php foreach ($avgDeaths as $key) {
+            series: [{
+                type: 'area',
+                name: 'Mortes',
+                data: [<?php foreach ($avgDeaths as $key) {
                             echo '[' . json_encode($key['Date']) . ',' . json_encode($key['Deaths']) . '],';
                         } ?>]
-        }]
+            }]
         });
     </script>
 
