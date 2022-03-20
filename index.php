b50b3f768cdbb510a69464a2be999a3c235d081f
Garufa1 <99214332+SrGarufa@users.noreply.github.com>
2022-03-20 05:07:50 -0300

    fix

--- index.php
+++ index.php
@@ -31,11 +31,8 @@ $date = date('Y-m-d', strtotime('-3 month'));
     <link rel="icon" href="/public/images/favicon.png">
     <link rel="stylesheet" href="public/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <?php
-    /*  header("refresh:1"); */
-    ?>
-    <!-- Highcharts.js -->
-    <!--     <script src="https://code.highcharts.com/highcharts.js"></script> -->
+
+
     <title>Covid Status</title>
 
     <script>
@@ -132,82 +129,6 @@ $date = date('Y-m-d', strtotime('-3 month'));
         <p>References and credits ~</p>
     </footer>
 
-
-
-    <!-- <aside class="nav">
-            <div id="nav-head" class="text-color">
-                <h3>Ajude a diminuir o contágio!</h3>
-                <div class="prev">
-                    <div class="prev-ico"><img src="/public/images/covid-icons/Patient.png" alt=""></div>
-                    <div class="prev-text">Use máscara sempre que possível, principalmente em locais fechados</div>
-                </div>
-                <div class="prev">
-                    <div class="prev-text">Fique em casa se você estiver indisposto</div>
-                    <div class="prev-ico"><img src="/public/images/covid-icons/home.png" alt=""></div>
-                </div>
-                <div class="prev">
-                    <div class="prev-ico"><img src="/public/images/covid-icons/hands.png" alt=""></div>
-                    <div class="prev-text">Lave as mãos com frequência usando sabão ou alcool gel</div>
-                </div>
-                <div class="prev">
-                    <div class="prev-text">Mantenha uma distância segura de outras pessoas, mesmo que não pareçam doentes</div>
-                    <div class="prev-ico"><img src="/public/images/covid-icons/distance.png" alt=""></div>
-                </div>
-                <div class="prev">
-                    <div class="prev-ico"><img src="/public/images/covid-icons/hospital.png" alt=""></div>
-                    <div class="prev-text">Caso apresente tosse, febre alta, falta de ar procure atendimento médico e ligue com antecedência para o posto de saúde, UPA ou pronto-socorro</div>
-                </div>
-
-            </div>
-
-            <hr>
-
-            <nav>
-                <?php foreach ($countries as $key) { ?>
-                    <a href="?country=<?= $key['Slug'] ?>">
-                        <div class="button-1">
-                            <div class="country">
-                                <ul>
-                                    <li>
-                                        <p class="text-color"><?= $key['Country'] ?></p>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="country-nmbrs">
-                                <ul>
-                                    <li>
-                                        <img src="public/images/flags/<?= $key['ISO2'] ?>.png" alt="" width="30px">
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </a>
-                <?php } ?>
-            </nav>
-        </aside> -->
-    <!--   <header class="head">
-            <h1>Covid Status</h1>
-            <button id="btn_">C</button>
-            <button id="dateTime" value="<?= $date ?>">3 Months</button>
-        </header> -->
-    <!-- <main class="grid container-main center">
-            <div class="head-content flex column">
-                <div id="graph-1"></div>
-                <div class="graph-descript">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
-            </div>
-            <div class="head-content flex column">
-                <div id="graph-2"></div>
-                <div class="graph-descript">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
-            </div>
-            <div class="head-content column">
-            <div id="graph-3"></div>
-            <div class="graph-descript">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
-            </div>
-            <div class="head-content flex column">
-                <div id="graph-4"></div>
-                <div class="graph-descript">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
-            </div>
-        </main> -->
 
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script>
