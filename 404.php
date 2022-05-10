ff775e46c39ff85001dea25687eff1250e314c56
Rodrigo Dalfre <67020557+rodrigodalfre@users.noreply.github.com>
2022-05-10 04:26:12 -0300

    link criado para card no 404

--- 404.php
+++ 404.php
@@ -24,17 +24,20 @@
         </div>
         <div id="cardArea" class="flex row space-between">
             <?php foreach ($countries as $key => $value) { ?>
+        
                 <div class="card404">
-                    <div class="cardFlag margin-auto"><?php echo ('<img src="./public/images/flags/' . $countries[$key]['ISO2'] . '.png" />') ?></div>
-                    <h2 class="text-color-3 marg"><?php echo $countries[$key]['Country'] ?></h2>
-                    <div>
-                        <ul class="text-color-3 marg">
-                            <li>Confirmed cases: <br/><div class="text-color-4"><?php echo $countries[$key]['Confirmed']; ?></div></li>
-                            <li>Active cases: <br/><div class="text-color-4"><?php echo $countries[$key]['Active']; ?></div></li>
-                            <li>Deaths: <br/><div class="text-color-4"><?php echo $countries[$key]['Deaths']; ?></div></li>
-                        </ul>
+                    <div class="cardFlag margin-auto"><?= ('<img src="./public/images/flags/' . $countries[$key]['ISO2'] . '.png" />') ?></div>
+                        <a href="/?country=<?= $countries[$key]['Slug'] ?>">
+                            <h2 class="text-color-3 marg"><?= $countries[$key]['Country'] ?></h2>
+                            <div>
+                                <ul class="text-color-3 marg">
+                                    <li>Confirmed cases: <br/><div class="text-color-4"><?php echo $countries[$key]['Confirmed']; ?></div></li>
+                                    <li>Active cases: <br/><div class="text-color-4"><?php echo $countries[$key]['Active']; ?></div></li>
+                                    <li>Deaths: <br/><div class="text-color-4"><?php echo $countries[$key]['Deaths']; ?></div></li>
+                                </ul>
+                            </div>
+                        </a>
                     </div>
-                </div>
             <?php } ?>
         </div>
     </main>
